12d551cb30b0c04b83bd596f0a6498d38f5d25fa
Soaple <inje@soaple.io>
2019-08-12 20:41:27 +0900

    Board updated for editing mode.

--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,12 @@
 // src/Board.js
 
 import React from 'react';
-import ReactResizeDetector from 'react-resize-detector';
-import {Responsive, WidthProvider} from 'react-grid-layout';
+import { Responsive, WidthProvider } from 'react-grid-layout';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 require('react-grid-layout/css/styles.css');
 require('react-resizable/css/styles.css');
 require('./static/css/react-grid-layout.css');
-
-const styles = theme => ({
-    root: {
-        width: '100%',
-        height: '100%',
-    },
-});
-
-const ROWS = 24;
 
 /**
  * React-Grid-Layout layout props
@@ -31,25 +21,30 @@ const RGL_LAYOUT_PROPS = {
     useCSSTransforms: true,
 };
 
+const NUM_OF_ROWS = 24;
+
 class Board extends React.Component {
     constructor (props) {
         super(props);
 
         this.state = {
             currentBreakpoint: 'lg',
-            isEditingMode: false,
             layoutUpdateFlag: true,
         }
     }
 
-    toggleEditingMode = () => {
-        let currentEditingMode = this.state.isEditingMode;
+    componentWillReceiveProps(nextProps) {
+        // Check if the editing mode is changed or not
+        // if (this.props.isEditingMode !== nextProps.isEditingMode) {
+        //     this.toggleEditingMode(nextProps.isEditingMode);
+        // }
+    }
 
-        let layouts = this.getLayouts(!currentEditingMode);
+    toggleEditingMode = (editingMode) => {
+        let layouts = this.getLayouts(editingMode);
         this.setState({
             layouts: {},
             layoutUpdateFlag: false,
-            isEditingMode: !currentEditingMode,
         }, () => {
             this.setState({
                 layouts: layouts,
@@ -78,11 +73,8 @@ class Board extends React.Component {
     }
 
     onBreakpointChange = (breakpoint) => {
-        // console.log('onBreakpointChange', breakpoint);
-
         this.setState({
             currentBreakpoint: breakpoint,
-            isEditingMode: breakpoint === 'lg' || breakpoint === 'md',
         });
     }
 
@@ -118,26 +110,19 @@ class Board extends React.Component {
     }
 
     render () {
-        const { isEditingMode } = this.state;
+        const { isEditingMode } = this.props;
+
+        const rowHeight = window.innerHeight / NUM_OF_ROWS;
 
         return (
-            <ReactResizeDetector
-                handleWidth
-                handleHeight
-                render={({ width, height }) => {
-                    const rowHeight = height / ROWS;
-
-                    return (
-                        <ResponsiveReactGridLayout
-                            {...RGL_LAYOUT_PROPS}
-                            rowHeight={rowHeight}
-                            layouts={this.getLayouts(isEditingMode)}
-                            onBreakpointChange={this.onBreakpointChange}
-                            onLayoutChange={this.onLayoutChange}>
-                            {this.props.children}
-                        </ResponsiveReactGridLayout>
-                    )
-                }} />
+            <ResponsiveReactGridLayout
+                {...RGL_LAYOUT_PROPS}
+                rowHeight={rowHeight}
+                layouts={this.getLayouts(isEditingMode)}
+                onBreakpointChange={this.onBreakpointChange}
+                onLayoutChange={this.onLayoutChange}>
+                {this.props.children}
+            </ResponsiveReactGridLayout>
         )
     }
 }
