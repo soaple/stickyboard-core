02a81effa76113440ce9837937fa4ca16402b6bc
Soaple <inje@soaple.io>
2019-09-02 14:51:44 +0900

    npmignore added, distribution setting updated.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'production',
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, 'lib'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
         libraryTarget: 'commonjs2'
     },
