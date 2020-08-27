730333d3cc0f720670e5df70f1c9494b9197b684
taewankang <37101468+taewankang@users.noreply.github.com>
2020-08-27 14:40:10 +0900

    Info

--- src/Sticker.js
+++ src/Sticker.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-
+import DescriptionIcon from '@material-ui/icons/Description';
 const showAnim = keyframes`
     from {
         opacity: 0;
@@ -21,7 +21,8 @@ const hideAnim = keyframes`
     }
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+`;
 
 const Shadow = styled.div`
     position: absolute;
@@ -69,36 +70,113 @@ const MenuButton = styled.button`
     color: #333333;
 `;
 
+const WriteInfo = styled(DescriptionIcon)`
+    position: absolute;
+    z-index: 8;
+    display: flex;
+    top: 10px;
+    left: 5px;
+    cursor:pointer;
+`
+
+const Container = styled.span`
+    display: flex;
+    margin-top: 30px;
+    width: calc(100%);
+    height: calc(100% - 50px);
+`
+
+const TextArea = styled.textarea`
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    resize: none;
+    margin-top: 30px;
+    border-radius: 10px;
+    outline: none;
+    margin: 10px;
+    z-index: 2;
+`
+
 class Sticker extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            content: '',
+            isMemoOn: false,        
+            isMoving: false,
+        };
+    }
 
-        this.state = {};
+    memoClick = (e) => {
+        this.setState({isMoving: !this.state.isMoving});
+        e.stopPropagation();
+    }
+
+    wrapperMouseDown = (e) => {
+        !this.state.isMemoOn && this.props.onMouseDown(e);
+    }
+
+    textClick = (e) => {
+        this.setState({isMemoOn: true})
+        e.stopPropagation();
+    }
+    
+    wrapperClick = (e) => {
+        this.setState({isMemoOn: false});
+    }
+
+    componentDidMount() {
+        this.setState({content: this.props.description[this.props.name]});
+    }
+
+    memoChange = (e) => {
+        this.setState({content: e.target.value});
+        this.props.descriptionChange(e);
     }
 
     render() {
         const {
             children,
+            name,
+            description,
+            descriptionChange,
             className,
             onChange,
             onDelete,
             ...other
         } = this.props;
 
-        const isEditingMode =
-            className && className.includes('react-resizable');
+        const isEditingMode = className && className.includes('react-resizable');
+        const {isMoving, content} = this.state;
 
         return (
-            <Wrapper {...other} className={className}>
-                <Shadow isEditingMode={isEditingMode} />
-                <Content>{children}</Content>
-
-                {isEditingMode && (
-                    <MenuContainer>
-                        <MenuButton onClick={onChange}>Change</MenuButton>
-                        <MenuButton onClick={onDelete}>Delete</MenuButton>
-                    </MenuContainer>
-                )}
+            <Wrapper 
+                {...other} 
+                className={className} 
+                onClick={this.wrapperClick} 
+                onMouseDown={this.wrapperMouseDown}>
+                { isEditingMode && <WriteInfo onClick={this.memoClick}/> }
+                    <Shadow isEditingMode={isEditingMode}/>
+                    <Content>
+                        {children}
+                        {isEditingMode && (
+                            <MenuContainer>
+                                <MenuButton onClick={onChange}>Change</MenuButton>
+                                <MenuButton onClick={onDelete}>Delete</MenuButton>
+                            </MenuContainer>
+                        )}
+                    </Content>
+                    {
+                        isMoving &&
+                        <Container>
+                            <TextArea
+                                value={content}
+                                name={name}
+                                onMouseDown={this.textClick}
+                                onChange={this.memoChange}/>
+                        </Container>
+                    }
             </Wrapper>
         );
     }
